e09d7a606a64c4d1085e674c9bcf0829f5fb5dcf
Dmitriy Kolesnikov <xxsnakerxx@gmail.com>
2022-11-23 17:00:15 +0100

    Add more logical properties support

--- src/__tests__/rtl.js
+++ src/__tests__/rtl.js
@@ -10,7 +10,12 @@ it('transforms start/end properties', () => {
   expect(transformCss([['end', '20px']])).toEqual({ end: 20 })
 })
 
-it('transforms marginStart/End properties', () => {
+it('transforms margin logical properties', () => {
+  expect(transformCss([['margin-inline', '10px 20px']])).toEqual({
+    marginStart: 10,
+    marginEnd: 20,
+  })
+
   expect(transformCss([['margin-inline-start', '10px']])).toEqual({
     marginStart: 10,
   })
@@ -20,6 +25,22 @@ it('transforms marginStart/End properties', () => {
   expect(transformCss([['margin-inline-start', '10%']])).toEqual({
     marginStart: '10%',
   })
+
+  expect(transformCss([['margin-block', '10px 20px']])).toEqual({
+    marginTop: 10,
+    marginBottom: 20,
+  })
+
+  expect(transformCss([['margin-block-start', '10px']])).toEqual({
+    marginTop: 10,
+  })
+  expect(transformCss([['margin-block-start', '10rem']])).toEqual({
+    marginTop: '10rem',
+  })
+  expect(transformCss([['margin-block-start', '10%']])).toEqual({
+    marginTop: '10%',
+  })
+
   expect(transformCss([['margin-inline-end', '20rem']])).toEqual({
     marginEnd: '20rem',
   })
@@ -29,35 +50,122 @@ it('transforms marginStart/End properties', () => {
   expect(transformCss([['margin-inline-end', '20px']])).toEqual({
     marginEnd: 20,
   })
+
+  expect(transformCss([['margin-block-end', '10px']])).toEqual({
+    marginBottom: 10,
+  })
+  expect(transformCss([['margin-block-end', '10rem']])).toEqual({
+    marginBottom: '10rem',
+  })
+  expect(transformCss([['margin-block-end', '10%']])).toEqual({
+    marginBottom: '10%',
+  })
 })
 
-it('transforms paddingStart/End properties', () => {
+it('transforms padding logical properties', () => {
+  expect(transformCss([['padding-inline', '10px 20px']])).toEqual({
+    paddingStart: 10,
+    paddingEnd: 20,
+  })
+
   expect(transformCss([['padding-inline-start', '10px']])).toEqual({
-    marginStart: 10,
+    paddingStart: 10,
   })
   expect(transformCss([['padding-inline-start', '10rem']])).toEqual({
-    marginStart: '10rem',
+    paddingStart: '10rem',
   })
   expect(transformCss([['padding-inline-start', '10%']])).toEqual({
-    marginStart: '10%',
+    paddingStart: '10%',
   })
+
+  expect(transformCss([['padding-block', '10px 20px']])).toEqual({
+    paddingTop: 10,
+    paddingBottom: 20,
+  })
+
+  expect(transformCss([['padding-block-start', '10px']])).toEqual({
+    paddingTop: 10,
+  })
+  expect(transformCss([['padding-block-start', '10rem']])).toEqual({
+    paddingTop: '10rem',
+  })
+  expect(transformCss([['padding-block-start', '10%']])).toEqual({
+    paddingTop: '10%',
+  })
+
   expect(transformCss([['padding-inline-end', '20rem']])).toEqual({
-    marginEnd: '20rem',
+    paddingEnd: '20rem',
   })
   expect(transformCss([['padding-inline-end', '20%']])).toEqual({
-    marginEnd: '20%',
+    paddingEnd: '20%',
   })
   expect(transformCss([['padding-inline-end', '20px']])).toEqual({
-    marginEnd: 20,
+    paddingEnd: 20,
+  })
+
+  expect(transformCss([['padding-block-end', '20rem']])).toEqual({
+    paddingBottom: '20rem',
+  })
+  expect(transformCss([['padding-block-end', '20%']])).toEqual({
+    paddingBottom: '20%',
+  })
+  expect(transformCss([['padding-block-end', '20px']])).toEqual({
+    paddingBottom: 20,
   })
 })
 
-it('transforms borderWidthStart/End properties', () => {
+it('transforms borderWidth logical properties', () => {
+  expect(transformCss([['border-inline-width', '10px 20px']])).toEqual({
+    borderStartWidth: 10,
+    borderEndWidth: 20,
+  })
+
+  expect(transformCss([['border-block-width', '10px 20px']])).toEqual({
+    borderTopWidth: 10,
+    borderBottomWidth: 20,
+  })
+
   expect(transformCss([['border-inline-start-width', '10px']])).toEqual({
     borderStartWidth: 10,
   })
 
+  expect(transformCss([['border-block-start-width', '10px']])).toEqual({
+    borderTopWidth: 10,
+  })
+
   expect(transformCss([['border-inline-end-width', '20px']])).toEqual({
     borderEndWidth: 20,
+  })
+
+  expect(transformCss([['border-block-end-width', '10px']])).toEqual({
+    borderBottomWidth: 10,
+  })
+})
+
+it('transforms position logical properties', () => {
+  expect(transformCss([['inset-inline-start', '10px']])).toEqual({
+    start: 10,
+  })
+
+  expect(transformCss([['inset-block-start', '10px']])).toEqual({
+    top: 10,
+  })
+
+  expect(transformCss([['inset-inline-end', '10px']])).toEqual({
+    end: 10,
+  })
+
+  expect(transformCss([['inset-block-end', '10px']])).toEqual({
+    bottom: 10,
+  })
+
+  expect(transformCss([['inset-block', '10px 20px']])).toEqual({
+    top: 10,
+    bottom: 20,
+  })
+
+  expect(transformCss([['inset-inline', '10px 20px']])).toEqual({
+    start: 10,
+    end: 20,
   })
 })
--- src/transforms/rtl.js
+++ src/transforms/rtl.js
@@ -1,4 +1,40 @@
-import { LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT } from '../tokenTypes'
+import { LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT, SPACE } from '../tokenTypes'
+
+const logicalFactory = ({
+  types = [LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT],
+  directions = ['Start', 'End'],
+  prefix = '',
+  suffix = '',
+}) => tokenStream => {
+  const values = []
+
+  values.push(tokenStream.expect(...types))
+
+  while (values.length < 2 && tokenStream.hasTokens()) {
+    tokenStream.expect(SPACE)
+    values.push(tokenStream.expect(...types))
+  }
+
+  tokenStream.expectEmpty()
+
+  const [first, second] = values
+
+  const keyFor = n => `${prefix}${directions[n]}${suffix}`
+
+  return {
+    [keyFor(0)]: first,
+    [keyFor(1)]: second,
+  }
+}
+
+export const marginInline = logicalFactory({
+  prefix: 'margin',
+})
+
+export const marginBlock = logicalFactory({
+  prefix: 'margin',
+  directions: ['Top', 'Bottom'],
+})
 
 export const marginInlineStart = tokenStream => ({
   marginStart: tokenStream.expect(
@@ -6,18 +42,58 @@ export const marginInlineStart = tokenStream => ({
   ),
 })
 
+export const marginBlockStart = tokenStream => ({
+  marginTop: tokenStream.expect(...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]),
+})
+
 export const marginInlineEnd = tokenStream => ({
   marginEnd: tokenStream.expect(...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]),
 })
 
-export const paddingInlineStart = tokenStream => ({
-  marginStart: tokenStream.expect(
+export const marginBlockEnd = tokenStream => ({
+  marginBottom: tokenStream.expect(
     ...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]
   ),
 })
 
+export const paddingInline = logicalFactory({
+  prefix: 'padding',
+})
+
+export const paddingBlock = logicalFactory({
+  prefix: 'padding',
+  directions: ['Top', 'Bottom'],
+})
+
+export const paddingInlineStart = tokenStream => ({
+  paddingStart: tokenStream.expect(
+    ...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]
+  ),
+})
+
+export const paddingBlockStart = tokenStream => ({
+  paddingTop: tokenStream.expect(...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]),
+})
+
 export const paddingInlineEnd = tokenStream => ({
-  marginEnd: tokenStream.expect(...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]),
+  paddingEnd: tokenStream.expect(...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]),
+})
+
+export const paddingBlockEnd = tokenStream => ({
+  paddingBottom: tokenStream.expect(
+    ...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]
+  ),
+})
+
+export const borderInlineWidth = logicalFactory({
+  prefix: 'border',
+  suffix: 'Width',
+})
+
+export const borderBlockWidth = logicalFactory({
+  prefix: 'border',
+  suffix: 'Width',
+  directions: ['Top', 'Bottom'],
 })
 
 export const borderInlineStartWidth = tokenStream => ({
@@ -26,8 +102,44 @@ export const borderInlineStartWidth = tokenStream => ({
   ),
 })
 
+export const borderBlockStartWidth = tokenStream => ({
+  borderTopWidth: tokenStream.expect(
+    ...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]
+  ),
+})
+
 export const borderInlineEndWidth = tokenStream => ({
   borderEndWidth: tokenStream.expect(
     ...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]
   ),
+})
+
+export const borderBlockEndWidth = tokenStream => ({
+  borderBottomWidth: tokenStream.expect(
+    ...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]
+  ),
+})
+
+export const insetInline = logicalFactory({
+  directions: ['start', 'end'],
+})
+
+export const insetBlock = logicalFactory({
+  directions: ['top', 'bottom'],
+})
+
+export const insetInlineStart = tokenStream => ({
+  start: tokenStream.expect(...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]),
+})
+
+export const insetBlockStart = tokenStream => ({
+  top: tokenStream.expect(...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]),
+})
+
+export const insetInlineEnd = tokenStream => ({
+  end: tokenStream.expect(...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]),
+})
+
+export const insetBlockEnd = tokenStream => ({
+  bottom: tokenStream.expect(...[LENGTH, UNSUPPORTED_LENGTH_UNIT, PERCENT]),
 })
